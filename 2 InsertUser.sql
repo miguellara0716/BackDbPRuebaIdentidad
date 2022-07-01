3d08a62d8c0b5882ca3c7f82427155a93270943e
miguellara0716 <50808457+miguellara0716@users.noreply.github.com>
2022-07-01 17:14:14 -0500

    first commit

---
+++ 2 InsertUser.sql
@@ -0,0 +1,14 @@
+USE [ReaderCSV]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([UserName]
+           ,[Password]
+           ,[State])
+     VALUES
+           ('QWRtaW4='
+           ,'QWRtaW4xMjM0Kw=='
+           ,1)
+GO
+
+
